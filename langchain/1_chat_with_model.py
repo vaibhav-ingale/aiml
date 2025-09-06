88bd671a13c707bc11f6e562e4f0e073a6b1567d
Vaibhav Ingale <vaibhav.d.ingale@gmail.com>
2025-09-06 14:41:04 -0700

    chat with local ollam models

--- langchain/1_chat_with_model.py
+++ langchain/1_chat_with_model.py
@@ -4,7 +4,7 @@ from langchain_ollama.llms import OllamaLLM
 
 template = """Question: {question}
 
-Answer: Understand like highschool student"""
+Answer: Understand like highschool student and answer in a concise manner."""
 
 prompt = ChatPromptTemplate.from_template(template)
 
@@ -18,7 +18,7 @@ if model_index < 0 or model_index >= len(models):
     raise ValueError("Invalid model index selected.")
 
 model_name = models[model_index].model
-model = OllamaLLM(model=model_name, temperature=0.1, max_tokens=1000)
+model = OllamaLLM(model=model_name, temperature=0.1, max_tokens=100)
 chain = prompt | model
 
 # Crete series of questions
@@ -26,13 +26,14 @@ questions = [
     "What is the capital of France?",
     "What is the largest mammal?",
     "What is the speed of light?",
-    "What is the meaning of life?",
     "What is the Fibonacci sequence?",
 ]
-for question in questions:
+print("=" * 40)
+# include qustion number as well
+for i, question in enumerate(questions, 1):
+    question = f"{i}. {question}"
     resp = chain.invoke({"question": question})
-    print(f"Question: {question}\nAnswer: {resp}\n")
-# Example usage
-print("Generating a specific question response:")
+    print(f"Question : {question}\nAnswer: {resp}\n")
+    print("-" * 40)
 
-print(resp)
+print("#" * 40)
